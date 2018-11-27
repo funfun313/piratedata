608b26283351ba6a58de4b857385548158653f6c
funfun313 <43829418+funfun313@users.noreply.github.com>
2018-11-26 19:02:40 -0600

    pirate data

--- piratedata.py
+++ piratedata.py
@@ -2,3 +2,11 @@ class Pirate:
     name = ""
     ship = ""
     fictional = ""
+    def getdict(self):
+        d = {"name":self.name,"ship":self.ship,"fictional":self.fictional}
+        return d
+    def loadfromdict(self,d):
+        self.name = d["name"]
+        self.ship = d["ship"]
+        self.fictional = d["fictional"]
+        
